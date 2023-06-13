5e09f539c76f8b142a52ba60be1a6453669087e0
HassanMunene <awanzihassan@gmail.com>
2023-06-13 20:57:51 +0300

    syntax corrections

--- app/registerBook/forms.py
+++ app/registerBook/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, DateField
 from wtforms.validators import DataRequired
 
 
 class RegisterBookForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
     author =StringField('Author', validators=[DataRequired()])
     isbn = StringField('ISBN', validators=[DataRequired()])
+    publisher = StringField('Publisher', validators=[DataRequired()])
+    publication_date = DateField('Publication Date')
