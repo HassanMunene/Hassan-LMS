3a392b048f2af36cc4573f6a10af17ee2cc2719c
HassanMunene <awanzihassan@gmail.com>
2023-06-15 10:08:39 +0300

    Book model creation

--- app/registerBook/views.py
+++ app/registerBook/views.py
@@ -1,9 +1,24 @@
 from .forms import RegisterBookForm
-from flask import render_template
+from flask import render_template, flash
 from . import regBook
+from app.models import Book
+from app import db
 
 
 @regBook.route('/registerBook', methods=['GET', 'POST'])
 def registerBook():
     form = RegisterBookForm()
+    if form.validate_on_submit():
+        book = Book()
+        book.title = form.title.data
+        book.author = form.author.data
+        book.isbn = form.isbn.data
+        book.publisher = form.publisher.data
+        book.publication_date = form.publication_date.data
+        book.genre = form.genre.data
+        book.condition = form.condition.data
+        db.session.add(book)
+        db.session.commit()
+        flash('Book registered successfully')
+        return redirect('main.dashboard')
     return render_template('regBook.html', form=form)
